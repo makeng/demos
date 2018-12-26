1bc9064d0160b11f50827ebc25899aaca405f060
马兆铿 <810768333@qq.com>
2018-12-26 16:45:55 +0800

    🌟：【promise】结果正确

--- functions/promiseChain.js
+++ functions/promiseChain.js
@@ -5,27 +5,37 @@
 
 function Man( str ) {
   this.say = function () {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        console.log(str);
-        resolve(str);
-      }, 500);
+    const promise = new Promise(( resolve, reject ) => {
+      if ( typeof str === "string" ) {
+        setTimeout(() => {
+          console.log(str);
+          resolve(str);
+        }, 500);
+      } else {
+        reject("not string");
+      }
     });
+    return promise;
   }
 }
 
 const man1 = new Man("1");
 const man2 = new Man("2");
-const man3 = new Man("3");
+const man3 = new Man(undefined);
+const man4 = new Man("4");
 
-async function chain( arr ) {
+async function chainPromise( arr ) {
   let cnt = arr.length;
   if ( cnt ) {
     const resArr = [];
-    for ( let i = 0; i < cnt; i++ ) {
-      const fn = arr[ i ];
-      const res = await fn();
-      resArr.push(res);
+    try {
+      for ( let i = 0; i < cnt; i++ ) {
+        const fn = arr[ i ];
+        const res = await fn();
+        resArr.push(res);
+      }
+    } catch ( e ) {
+      console.error(e);
     }
     return resArr;
   } else {
@@ -33,7 +43,7 @@ async function chain( arr ) {
   }
 }
 
-chain([ man1.say, man2.say, man3.say ]).then(res => {
+chainPromise([ man1.say, man2.say, man3.say ]).then(res => {
   console.log(res);
 });
 
--- functions/requestChain.js
+++ functions/requestChain.js
@@ -3,30 +3,56 @@
 * date:2018-12-25
 * ----------------------------------------------------------------------------------*/
 
+// 拦截器的存储
 function InterceptorManager() {
-
+  this.promiseArr = [];
+  this.config = {};
+  this.fulfilled = undefined;
+  this.rejected = undefined;
+  this.use = function ( success, fail ) {
+    this.fulfilled = () => {
+      if ( success ) {
+        return Promise.resolve(success(this.config));
+      } else {
+        return fail();
+      }
+    }
+    this.rejected = () => {
+      return fail();
+    }
+    this.promiseArr.push(this.fulfilled); // 支持多个任务
+    this.promiseArr.push(this.rejected); // 支持多个任务
+  }
 }
 
-function Axios( instanceConfig ) {
-  this.request = function ( config ) {
-    // 返回
-    const chain = [];
-    let promise = undefined;
-    /*    while ( chain.length ) {
-          promise = new Promise().then(chain.shift(), chain.shift());
-        }*/
-    promise = new Promise(resolve => {
-      resolve(`发送内容${JSON.stringify(config)}`);
-    });
-    return promise;
-  };
+function fetch( config ) {
+  return Promise.resolve(`发送数据${JSON.stringify(config)}`)
+}
 
+function Axios() {
   this.interceptors = {
     request: new InterceptorManager(),
     response: new InterceptorManager()
   };
+
+  this.request = function ( config ) {
+    const chain = [ fetch, undefined ];
+    let promise = Promise.resolve(config);
+    // 压入任务
+    this.interceptors.request.config = config;
+    this.interceptors.response.config = config;
+    console.log(chain);
+    /*    while ( chain.length ) {
+          promise = promise.then(chain.shift(), chain.shift());
+        }*/
+    return promise
+      .then(this.interceptors.request.fulfilled, this.interceptors.request.rejected)
+      .then(fetch, undefined)
+      .then(this.interceptors.response.fulfilled, this.interceptors.response.rejected)
+  };
 }
 
+// request的包装
 [ 'post', 'delete', 'get', 'head', 'options' ].forEach(method => {
   Axios.prototype[ method ] = function ( url, config ) {
     return this.request({
@@ -37,7 +63,32 @@ function Axios( instanceConfig ) {
 });
 
 const axios = new Axios();
-
-axios.post("test").then(res => {
-  console.log(res);
-});
+// 拦截
+axios.interceptors.request.use(
+  function ( config ) {
+    console.log("请求前拦截", config);
+    return config;
+  },
+  function ( error ) {
+    console.log("请求前失败", error);
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  function ( config ) {
+    console.log("请求后拦截", config);
+    return config;
+  },
+  function ( error ) {
+    console.log("请求后失败", error);
+    return Promise.reject(error);
+  }
+);
+// 调用
+axios.post("test")
+  .then(res => {
+    console.log("发送成功", res);
+  })
+  .catch(err => {
+    console.log("🙅")
+  });
