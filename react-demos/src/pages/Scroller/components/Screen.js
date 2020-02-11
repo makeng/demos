5fbac6de157762149f3426c9b8d6d13a081d377b
makeng <mazhaokeng@orvibo.com>
2020-02-11 17:02:47 +0800

    🌟：【React】「无限滚动」代码优化。

--- react-demos/src/pages/Scroller/components/Screen.js
+++ react-demos/src/pages/Scroller/components/Screen.js
@@ -1,89 +1,115 @@
 /*----------------------------------------------------------------------------------
-* about:数据对象
+* about:屏幕组件
 * author:马兆铿
 * date:
 * ----------------------------------------------------------------------------------*/
 /* ----------------------------------------- 开始运行 ----------------------------------------- */
-import React, {PureComponent, PropTypes} from "react";
-import Film from "../js/Film";
-import {List} from 'antd-mobile';
-import {on} from "../../../utils/lib/dom";
-import "../../../style/pages/Scroller.less";
+import React from 'react'
+import { DataList } from '../js/DataList'
+import { List } from 'antd-mobile'
+import { on } from '../../../utils/lib/dom'
+import '../../../style/pages/Scroller.less'
 
-const Item = List.Item;
-const Brief = Item.Brief;
+const Item = List.Item
+const Brief = Item.Brief
 
-const ITEM_HEIGHT = 40;
+const ITEM_HEIGHT = 40
+const APPEND_ITEM_CNT = 50 // 每次增加的数量
 
 //
 class Screen extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      film: new Film(),
-      screen: {start: 0, end: 999}
-    };
+      dataList: new DataList(),
+      screen: { start: 0, end: 999 }
+    }
   }
 
   /* ----------------------------------------- 生命周期 ----------------------------------------- */
-  componentDidMount() {
-    const el = document.getElementsByClassName("scroller")[0];
-    console.log(el);
-    on(el, "scroll", e => {
-      this.onScroll(e);
-    });
+  componentDidMount () {
+    this.dataListAppendMoreItems()
+    this.attachScrollEvent()
   }
 
   /* ----------------------------------------- 自定义方法 ----------------------------------------- */
+  /**
+   * 绑定滚动事件
+   */
+  attachScrollEvent () {
+    const el = document.getElementsByClassName('scroller')[0]
+    on(el, 'scroll', e => {
+      this.onScroll(e)
+    })
+  }
 
+  /**
+   * 设置可视窗口参数
+   */
+  setScreen (start, end) {
+    const screen = {
+      start: parseInt(start),
+      end: parseInt(end)
+    }
+    this.setState({ screen })
+  }
+
+  /**
+   * 加载更多数据
+   */
+  dataListAppendMoreItems () {
+    const { dataList } = this.state
+    dataList.appendDataItem(APPEND_ITEM_CNT)
+    console.log(dataList)
+    this.setState({ dataList })
+  }
 
   /* ----------------------------------------- 绑定方法 ----------------------------------------- */
-  onScroll(e) {
-    const target = e.target;
-    const scrollHeight = target.scrollHeight; // 真正高度
-    const offsetHeight = target.offsetHeight; //窗口高度
-    const scrollTop = target.scrollTop; // 滚动了多少
+  onScroll (e) {
+    const target = e.target
+    const scrollHeight = target.scrollHeight // 真正高度
+    const offsetHeight = target.offsetHeight //窗口高度
+    const scrollTop = target.scrollTop // 滚动了多少
 
-    const screen = {
-      start: parseInt(target.scrollTop / ITEM_HEIGHT),
-      end: parseInt((target.scrollTop + target.offsetHeight) / ITEM_HEIGHT)
-    };
-
-    this.setState({screen});
+    // 设置屏幕
+    const screenStart = target.scrollTop / ITEM_HEIGHT
+    const screenEnd = (target.scrollTop + target.offsetHeight) / ITEM_HEIGHT
+    this.setScreen(screenStart, screenEnd)
 
     // 触底
     if (offsetHeight + scrollTop + 100 >= scrollHeight) {
-      const {film} = this.state;
-      film.getMore();
-      this.setState({film});
+      this.dataListAppendMoreItems()
     }
   }
 
   /* ----------------------------------------- 渲染 ----------------------------------------- */
-  render() {
-    const {film, screen} = this.state;
-    const OFFSET = 3;
+  render () {
+    const { dataList, screen } = this.state
+    const OFFSET = 3
     return (
       <div className="scroller">
         <List id="scroll-list" onScroll={this.onScroll}>
           {
-            film.list.map(item =>
+            dataList.content.map(item =>
               <div
-                style={{height: `${ITEM_HEIGHT}px`}}
+                style={{ height: `${ITEM_HEIGHT}px` }}
                 key={item.id}
                 className="item-wrap"
               >
                 {
+                  /* 可视窗口的数据才能显示 */
                   (item.id <= screen.end - OFFSET && item.id >= screen.start + OFFSET) &&
-                  <Item extra={"我的顺序是：" + item.id}>{item.desc}</Item>
+                  <Item extra={'数据'}>
+                    {item.desc}
+                  </Item>
                 }
               </div>
             )
           }
         </List>
       </div>
-    );
+    )
   }
 }
 
-export default Screen;
+export default Screen
--- react-demos/src/pages/Scroller/index.js
+++ react-demos/src/pages/Scroller/index.js
@@ -1,22 +1,22 @@
-/* -----------------------------------------
-about：此文件是登录页面
-author：苏昱霖
-date：2017年12月18日
------------------------------------------*/
+/*----------------------------------------------------------------------------------
+* about:页面对象
+* author:马兆铿
+* date:
+* ----------------------------------------------------------------------------------*/
 /* ----------------------------------------- 开始运行 ----------------------------------------- */
-import React, {PureComponent, PropTypes} from "react";
-import "../../style/pages/Scroller.less";
-import Screen from "./components/Screen"
+import React from 'react'
+import '../../style/pages/Scroller.less'
+import Screen from './components/Screen'
 
 //
 class Scroller extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor (props) {
+    super(props)
+    this.state = {}
   }
 
   /* ----------------------------------------- 生命周期 ----------------------------------------- */
-  componentDidMount() {
+  componentDidMount () {
   }
 
   /* ----------------------------------------- 自定义方法 ----------------------------------------- */
@@ -25,14 +25,14 @@ class Scroller extends React.Component {
   /* ----------------------------------------- 绑定方法 ----------------------------------------- */
 
   /* ----------------------------------------- 渲染 ----------------------------------------- */
-  render() {
+  render () {
     return (
       <div className="page">
         <header>无限滚动</header>
-          <Screen/>
+        <Screen />
       </div>
-    );
+    )
   }
 }
 
-export default Scroller;
+export default Scroller
