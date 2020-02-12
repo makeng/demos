5726a5a78b2d1540d41656679bba68d6fae8b79b
makeng <mazhaokeng@orvibo.com>
2020-02-12 18:44:07 +0800

    🌟：【React】「无限滚动」页面美化。

--- react-demos/src/pages/ScrollerEqualHeightDemo/index.js
+++ react-demos/src/pages/ScrollerEqualHeightDemo/index.js
@@ -10,7 +10,7 @@ import Scroller from './components/Scroller'
 import { DataList } from './js/DataList'
 
 const APPEND_ITEM_CNT = 50 // 每次增加的数量
-const ITEM_HEIGHT = 40
+const ITEM_HEIGHT = 100
 
 //
 class ScrollerEqualHeightDemo extends React.Component {
@@ -48,7 +48,7 @@ class ScrollerEqualHeightDemo extends React.Component {
     const { dataList, screen } = this.state
     return (
       <div className="page">
-        <header className="page__header">无限滚动</header>
+        <header className="page__header">无限滚动：不卡顿</header>
         <Scroller
           itemHeight={ITEM_HEIGHT}
           onScrollToBottom={this.onScrollerScrollToBottom}
@@ -60,10 +60,16 @@ class ScrollerEqualHeightDemo extends React.Component {
                 key={item.id}
                 className="content-item"
               >
-                {/* item 显示内容 */}
-                <div>{item.desc}</div>
-                <p>我是一个等高的 item</p>
-                <p>最大高度是{ITEM_HEIGHT}</p>
+                {/* item 显示的内容 */}
+                <div className="content-item__avatar">
+                  <img
+                    src="https://s.cn.bing.net/th?q=%e6%98%8e%e6%98%9f&dpr=2&pid=MSNJVFeeds&mkt=zh-CN&adlt=moderate" />
+                </div>
+                <div className="content-item__desc">
+                  <div>{item.desc}</div>
+                  <p>我是一个等高的 item</p>
+                  <p>最大高度是{ITEM_HEIGHT}</p>
+                </div>
               </div>
             )
           }
