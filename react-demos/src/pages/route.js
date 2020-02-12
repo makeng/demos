c7516b2342ee53fc0809d62e4e74a213eb0d022c
makeng <810768333@qq.com>
2020-02-12 16:44:16 +0800

    🌟：【React】「无限滚动」模块化。

--- react-demos/src/pages/route.js
+++ react-demos/src/pages/route.js
@@ -4,7 +4,7 @@ import "../style/reset.less";
 /*-------------------------------------------- 路由对象 --------------------------------------------*/
 //组件
 const HomePage = asyncRoute(() => import("./HomePage"));
-const Scroller = asyncRoute(() => import("./Scroller"));
+const Scroller = asyncRoute(() => import("./ScrollerEqualHeightDemo"));
 const FilesDownload = asyncRoute(() => import("./FilesDownload"));
 const ChartHandle = asyncRoute(() => import("./ChartHandle"));
 //route
@@ -15,8 +15,8 @@ export const route = [
     component: HomePage
   },
   {
-    name: "无限滚动",
-    path: "/Scroller",
+    name: "无限滚动-等高掏空",
+    path: "/ScrollerEqualHeightDemo",
     component: Scroller
   },
   {
@@ -35,7 +35,7 @@ export const route = [
 // 为了消除 react-hot-loader code split 没有更新的BUG，作用是热加载。
 if (process.env.NODE_ENV !== "production") {
   // ... 有多少异步模块就 require 多少，不可以使用变量或者对象来require
-  require("./Scroller");
+  require("./ScrollerEqualHeightDemo");
   require("./HomePage");
   require("./FilesDownload");
   require("./ChartHandle");
