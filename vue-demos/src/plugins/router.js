242eda5ef84ca9fe612c420c73c47e6e3dc63813
makeng <mazhaokeng@orvibo.com>
2020-05-19 17:25:31 +0800

    🌟：【Less】文件初始化。

--- vue-demos/src/plugins/router.js
+++ vue-demos/src/plugins/router.js
@@ -1,10 +1,12 @@
 // 页面
 import Home from '../pages/Home'
 import Cart from '../pages/Cart'
+import Less from '../pages/Less'
 
 const routes = [
   { chName: '首页', path: '/', component: Home, desc: '页面的列表' },
   { chName: '购物车', path: '/cart', component: Cart, desc: '状态管理、数值计算' },
+  { chName: 'Less学习', path: '/less', component: Less, desc: 'Less基础用法' },
 ]
 
 export {
