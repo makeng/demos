da80f2aab8d2d2aaf67ee752bbadaeea125323c2
makeng <810768333@qq.com>
2022-07-16 20:07:23 +0800

    🏗️ replace webpack with vite

--- fe-vue/src/App.vue
+++ fe-vue/src/App.vue
@@ -1,26 +1,31 @@
-<template>
-  <div id="app">
-    <router-view/>
-    <debug-block position="bottom" />
-  </div>
-</template>
-
-<script>
-  import { DebugBlock } from './components'
-
-  export default {
-    name: 'app',
-    components: { DebugBlock }
-  }
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="less">
-  @import "./assets/style/common.less";
-  @import "assets/style/var.less";
-  @import "assets/style/vant.reset.less";
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-  #app {
-    flex: 1;
-    background-color: @color-bg;
-  }
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
